b9a025d79628664ee314c303d3e0c2bbb2dd126f
KevinAlmada <kevinalmada81@gmail.com>
2021-09-24 16:25:06 -0300

    a

---
+++ generator/src/data/dataInser.sql
@@ -0,0 +1,48 @@
+insert into users (name,password) values
+("kevin","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into users (name,password) values
+("joni","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into users (name,password) values
+("aime","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into users (name,password) values
+("tobi","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into users (name,password) values
+("maca","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into users (name,password) values
+("gonza","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",1);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",1);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",1);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",2);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",2);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",2);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",3);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",3);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",3);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",4);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",4);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",4);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",5);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",5);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",5);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",6);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",6);
+insert into user_recipes (name,description,user_id) values
+("prueba"," es una prueba",6);
