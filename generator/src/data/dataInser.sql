f8cebae5931e9a8d01faa9266c28695fd337be2b
KevinAlmada <kevinalmada81@gmail.com>
2021-09-24 16:58:16 -0300

    insert into muchas cosas

--- generator/src/data/dataInser.sql
+++ generator/src/data/dataInser.sql
@@ -10,39 +10,190 @@ insert into users (name,password) values
 ("maca","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
 insert into users (name,password) values
 ("gonza","$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92");
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",1);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",1);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",1);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",2);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",2);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",2);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",3);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",3);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",3);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",4);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",4);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",4);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",5);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",5);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",5);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",6);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",6);
-insert into user_recipes (name,description,user_id) values
+insert into userRecipes (name,description,user_id) values
 ("prueba"," es una prueba",6);
+/*  */
+insert into images (name,user_recipes_id) values
+("default-image.png",1);
+insert into images (name,user_recipes_id) values
+("default-image.png",1);
+insert into images (name,user_recipes_id) values
+("default-image.png",1);
+insert into images (name,user_recipes_id) values
+("default-image.png",2);
+insert into images (name,user_recipes_id) values
+("default-image.png",2);
+insert into images (name,user_recipes_id) values
+("default-image.png",2);
+insert into images (name,user_recipes_id) values
+("default-image.png",3);
+insert into images (name,user_recipes_id) values
+("default-image.png",3);
+insert into images (name,user_recipes_id) values
+("default-image.png",3);
+insert into images (name,user_recipes_id) values
+("default-image.png",4);
+insert into images (name,user_recipes_id) values
+("default-image.png",4);
+insert into images (name,user_recipes_id) values
+("default-image.png",4);
+insert into images (name,user_recipes_id) values
+("default-image.png",5);
+insert into images (name,user_recipes_id) values
+("default-image.png",5);
+insert into images (name,user_recipes_id) values
+("default-image.png",5);
+insert into images (name,user_recipes_id) values
+("default-image.png",6);
+insert into images (name,user_recipes_id) values
+("default-image.png",6);
+insert into images (name,user_recipes_id) values
+("default-image.png",6);
+/*  */
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 1",1);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 1",1);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 1",1);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 1",2);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 1",2);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 1",2);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 1",3);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 1",3);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 1",3);
+
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 2",4);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 2",4);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 2",4);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 2",5);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 2",5);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 2",5);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 2",6);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 2",6);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 2",6);
+
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 3",7);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 3",7);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 3",7);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 3",8);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 3",8);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 3",8);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 3",9);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 3",9);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 3",9);
+
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 4",10);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 4",10;
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 4",10);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 4",11);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 4",11);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 4",11);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 4",12);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 4",12);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 4",12);
+
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 5",13);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 5",13);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 5",13);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 5",14);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 5",14);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 5",14);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 5",15);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 5",15);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 5",15);
+
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 1 user 6",16);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 1 user 6",16);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 1 user 6",16);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 2 user 6",17);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 2 user 6",17);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 2 user 6",17);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 1 receta 3 user 6",18);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 2 receta 3 user 6",18);
+insert into ingredients (name,user_recipes_id) values
+("ingrediente 3 receta 3 user 6",18);
--- generator/src/data/recipes_db.sql
+++ generator/src/data/recipes_db.sql
@@ -28,8 +28,8 @@ CREATE TABLE `images` (
   `user_recipes_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `images_FK` (`user_recipes_id`),
-  CONSTRAINT `images_FK` FOREIGN KEY (`user_recipes_id`) REFERENCES `user_recipes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `images_FK` FOREIGN KEY (`user_recipes_id`) REFERENCES `userrecipes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'default-image.png',1),(2,'default-image.png',1),(3,'default-image.png',1),(4,'default-image.png',2),(5,'default-image.png',2),(6,'default-image.png',2),(7,'default-image.png',3),(8,'default-image.png',3),(9,'default-image.png',3),(10,'default-image.png',4),(11,'default-image.png',4),(12,'default-image.png',4),(13,'default-image.png',5),(14,'default-image.png',5),(15,'default-image.png',5),(16,'default-image.png',6),(17,'default-image.png',6),(18,'default-image.png',6);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,8 +55,8 @@ CREATE TABLE `ingredients` (
   `user_recipes_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ingredients_FK` (`user_recipes_id`),
-  CONSTRAINT `ingredients_FK` FOREIGN KEY (`user_recipes_id`) REFERENCES `user_recipes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `ingredients_FK` FOREIGN KEY (`user_recipes_id`) REFERENCES `userrecipes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,17 +65,18 @@ CREATE TABLE `ingredients` (
 
 LOCK TABLES `ingredients` WRITE;
 /*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES (1,'ingrediente 1 receta 1 user 1',1),(2,'ingrediente 2 receta 1 user 1',1),(3,'ingrediente 3 receta 1 user 1',1),(4,'ingrediente 1 receta 2 user 1',2),(5,'ingrediente 2 receta 2 user 1',2),(6,'ingrediente 3 receta 2 user 1',2),(7,'ingrediente 1 receta 3 user 1',3),(8,'ingrediente 2 receta 3 user 1',3),(9,'ingrediente 3 receta 3 user 1',3),(10,'ingrediente 1 receta 1 user 2',4),(11,'ingrediente 2 receta 1 user 2',4),(12,'ingrediente 3 receta 1 user 2',4),(13,'ingrediente 1 receta 2 user 2',5),(14,'ingrediente 2 receta 2 user 2',5),(15,'ingrediente 3 receta 2 user 2',5),(16,'ingrediente 1 receta 3 user 2',6),(17,'ingrediente 2 receta 3 user 2',6),(18,'ingrediente 3 receta 3 user 2',6),(19,'ingrediente 1 receta 1 user 3',7),(20,'ingrediente 2 receta 1 user 3',7),(21,'ingrediente 3 receta 1 user 3',7),(22,'ingrediente 1 receta 2 user 3',8),(23,'ingrediente 2 receta 2 user 3',8),(24,'ingrediente 3 receta 2 user 3',8),(25,'ingrediente 1 receta 3 user 3',9),(26,'ingrediente 2 receta 3 user 3',9),(27,'ingrediente 3 receta 3 user 3',9),(28,'ingrediente 1 receta 1 user 4',10),(29,'ingrediente 2 receta 1 user 4',10),(30,'ingrediente 3 receta 1 user 4',10),(31,'ingrediente 1 receta 2 user 4',11),(32,'ingrediente 2 receta 2 user 4',11),(33,'ingrediente 3 receta 2 user 4',11),(34,'ingrediente 1 receta 3 user 4',12),(35,'ingrediente 2 receta 3 user 4',12),(36,'ingrediente 3 receta 3 user 4',12),(37,'ingrediente 1 receta 1 user 5',13),(38,'ingrediente 2 receta 1 user 5',13),(39,'ingrediente 3 receta 1 user 5',13),(40,'ingrediente 1 receta 2 user 5',14),(41,'ingrediente 2 receta 2 user 5',14),(42,'ingrediente 3 receta 2 user 5',14),(43,'ingrediente 1 receta 3 user 5',15),(44,'ingrediente 2 receta 3 user 5',15),(45,'ingrediente 3 receta 3 user 5',15),(46,'ingrediente 1 receta 1 user 6',16),(47,'ingrediente 2 receta 1 user 6',16),(48,'ingrediente 3 receta 1 user 6',16),(49,'ingrediente 1 receta 2 user 6',17),(50,'ingrediente 2 receta 2 user 6',17),(51,'ingrediente 3 receta 2 user 6',17),(52,'ingrediente 1 receta 3 user 6',18),(53,'ingrediente 2 receta 3 user 6',18),(54,'ingrediente 3 receta 3 user 6',18);
 /*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user_recipes`
+-- Table structure for table `userrecipes`
 --
 
-DROP TABLE IF EXISTS `user_recipes`;
+DROP TABLE IF EXISTS `userrecipes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_recipes` (
+CREATE TABLE `userrecipes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `description` varchar(100) NOT NULL,
@@ -82,16 +84,17 @@ CREATE TABLE `user_recipes` (
   PRIMARY KEY (`id`),
   KEY `user_recipes_FK` (`user_id`),
   CONSTRAINT `user_recipes_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_recipes`
+-- Dumping data for table `userrecipes`
 --
 
-LOCK TABLES `user_recipes` WRITE;
-/*!40000 ALTER TABLE `user_recipes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_recipes` ENABLE KEYS */;
+LOCK TABLES `userrecipes` WRITE;
+/*!40000 ALTER TABLE `userrecipes` DISABLE KEYS */;
+INSERT INTO `userrecipes` VALUES (1,'prueba',' es una prueba',1),(2,'prueba',' es una prueba',1),(3,'prueba',' es una prueba',1),(4,'prueba',' es una prueba',2),(5,'prueba',' es una prueba',2),(6,'prueba',' es una prueba',2),(7,'prueba',' es una prueba',3),(8,'prueba',' es una prueba',3),(9,'prueba',' es una prueba',3),(10,'prueba',' es una prueba',4),(11,'prueba',' es una prueba',4),(12,'prueba',' es una prueba',4),(13,'prueba',' es una prueba',5),(14,'prueba',' es una prueba',5),(15,'prueba',' es una prueba',5),(16,'prueba',' es una prueba',6),(17,'prueba',' es una prueba',6),(18,'prueba',' es una prueba',6);
+/*!40000 ALTER TABLE `userrecipes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -103,10 +106,10 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,6 +118,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'kevin','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92'),(2,'joni','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92'),(3,'aime','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92'),(4,'tobi','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92'),(5,'maca','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92'),(6,'gonza','$2a$10$2TD7MIx.QoTBNPQFFtnV9uKBkOJfCUWK5qzNOl9S/Nf1GNs.6lS92');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-24 15:08:21
+-- Dump completed on 2021-09-24 16:57:08
