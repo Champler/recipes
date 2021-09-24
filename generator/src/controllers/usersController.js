cf9295440dd42ad63608c38111de58eaf003f7d7
SorakaTop <alcocergonzalomatias@gmail.com>
2021-09-24 18:43:40 -0300

    pull

--- generator/src/controllers/usersController.js
+++ generator/src/controllers/usersController.js
@@ -23,7 +23,7 @@ module.exports = {
     login: (req, res) => {
         //vista
         res.render('login',{
-            title: 'Logueate'
+            title: 'Logueate campion'
         })
     },
     loginProcess: (req, res) => {
