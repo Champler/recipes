06a6420cfc33bf25d47c365ee20e7c48c64a30cb
SorakaTop <alcocergonzalomatias@gmail.com>
2021-09-24 15:56:07 -0300

    holis

--- generator/src/controllers/usersController.js
+++ generator/src/controllers/usersController.js
@@ -4,12 +4,14 @@ const db = require('../database/models')
 module.exports = {
     register: (req, res) => {
         //vista
+        res.render('register')
     },
     registerProcess: (req, res) => {
         //crud
     },
     login: (req, res) => {
         //vista
+        res.render('login')
     },
     loginProcess: (req, res) => {
         //crud
