22ee9ebcba874b6af028f409e50d472f4beae83c
aime <aimeagustinagarces@gmail.com>
2021-09-24 17:30:18 -0300

    cards sin estilo, mandale ahora tranquilo perro

--- generator/src/controllers/indexController.js
+++ generator/src/controllers/indexController.js
@@ -9,7 +9,7 @@ module.exports = {
                 {association: 'user'}]
             })
         .then((recipes)=>{
-            res.send(recipes)
+           
             res.render('index',{
                 recipes
             })
